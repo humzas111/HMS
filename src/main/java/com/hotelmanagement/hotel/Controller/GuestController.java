1c5cb704aff107889914d93b5fb557eacc185626
unknown <humzasohail111@gmail.com>
2023-12-09 19:28:25 +0500

    Controllers of Guest and Staff

--- src/main/java/com/hotelmanagement/hotel/Controller/GuestController.java
+++ src/main/java/com/hotelmanagement/hotel/Controller/GuestController.java
@@ -1,4 +1,44 @@
 package com.hotelmanagement.hotel.Controller;
 
+
+import com.hotelmanagement.hotel.Entity.Guest;
+import com.hotelmanagement.hotel.Service.GuestService.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/api")
 public class GuestController {
+    @Autowired
+    private GuestService guestService;
+
+    public GuestController(GuestService guestService) {
+        this.guestService = guestService;
+    }
+
+    @GetMapping("/allGuest")
+    public List<Guest> getAllStaff(){
+        return guestService.findAll();
+    }
+
+    @PostMapping("/addGuest")
+    public Guest addStaff(@RequestBody Guest theGuest){
+        Guest dbGuest = guestService.save(theGuest);
+        return dbGuest;
+    }
+
+    @PutMapping("/Guest")
+    public Guest updateStaff(@RequestBody Guest theGuest){
+        Guest dbGuest = guestService.save(theGuest);
+        return dbGuest;
+    }
+
+    @DeleteMapping("/staffs/{id}")
+    public String deleteStaff(@PathVariable int id){
+        Guest theGuest = guestService.findById(id);
+        guestService.deleteById(id);
+        return "Guest Deleted: "+ id;
+    }
+
 }
--- src/main/java/com/hotelmanagement/hotel/Controller/StaffController.java
+++ src/main/java/com/hotelmanagement/hotel/Controller/StaffController.java
@@ -1,4 +1,46 @@
 package com.hotelmanagement.hotel.Controller;
 
+
+import com.hotelmanagement.hotel.Entity.Room;
+import com.hotelmanagement.hotel.Entity.Staff;
+import com.hotelmanagement.hotel.Service.RoomService.RoomService;
+import com.hotelmanagement.hotel.Service.StaffService.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/api")
 public class StaffController {
+    @Autowired
+    private StaffService staffService;
+
+    public StaffController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping("/allstaff")
+    public List<Staff> getAllStaff(){
+        return staffService.findAll();
+    }
+
+    @PostMapping("/addStaff")
+    public Staff addStaff(@RequestBody Staff theStaff){
+        Staff dbStaff = staffService.save(theStaff);
+        return dbStaff;
+    }
+
+    @PutMapping("/staff")
+    public Staff updateStaff(@RequestBody Staff theStaff){
+        Staff dbRoom = staffService.save(theStaff);
+        return dbRoom;
+    }
+
+    @DeleteMapping("/staffs/{id}")
+    public String deleteStaff(@PathVariable int id){
+        Staff theStaff = staffService.findById(id);
+        staffService.deleteById(id);
+        return "Staff Deleted: "+ id;
+    }
+
 }
